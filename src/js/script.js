34d1cabd85e21ce8d6b6b53da231efbd3e3abd88
Dodge <dorefrati@gmail.com>
2018-11-14 21:32:41 +0200

    Added brightness to steps. also added snapshot

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,11 @@
+// painterBot v0.2 by DodgeCode
+// This is an experimental artwork paint by randomness
+
+// Config
+let pixelSize = 1; // Pixels
+let gridSize = "600"; // Pixels
+let stepInterval = 0.1; // Milliseconds
+
 // Colors
 let colors = [
 	"#044A56", // Background
@@ -7,11 +15,10 @@ let colors = [
 	"#45B0A6"
 ];
 
+// Directions
 directions = ['up', 'down', 'left', 'right'];
 
 // Grid
-let pixelSize = 1;
-let gridSize = "600";
 let gridCenter = gridSize/2;
 let x = gridCenter;
 let y = gridCenter;
@@ -35,6 +42,45 @@ function download() {
     this.href = dt;
 };
 downloadLnk.addEventListener('click', download, false);
+
+// RGB to HSL
+function rgb2hsl(rgbArr){
+    var r1 = rgbArr[0] / 255;
+    var g1 = rgbArr[1] / 255;
+    var b1 = rgbArr[2] / 255;
+ 
+    var maxColor = Math.max(r1,g1,b1);
+    var minColor = Math.min(r1,g1,b1);
+    //Calculate L:
+    var L = (maxColor + minColor) / 2 ;
+    var S = 0;
+    var H = 0;
+    if(maxColor != minColor){
+        //Calculate S:
+        if(L < 0.5){
+            S = (maxColor - minColor) / (maxColor + minColor);
+        }else{
+            S = (maxColor - minColor) / (2.0 - maxColor - minColor);
+        }
+        //Calculate H:
+        if(r1 == maxColor){
+            H = (g1-b1) / (maxColor - minColor);
+        }else if(g1 == maxColor){
+            H = 2.0 + (b1 - r1) / (maxColor - minColor);
+        }else{
+            H = 4.0 + (r1 - g1) / (maxColor - minColor);
+        }
+    }
+ 
+    L = L * 100;
+    S = S * 100;
+    H = H * 60;
+    if(H<0){
+        H += 360;
+    }
+    var result = [H, S, L];
+    return result;
+}
 
 // Brush
 class Brush {
@@ -102,6 +148,17 @@ class Brush {
     getCurrentPos(){
     	return {'x':this.x, 'y':this.y};
     }
+
+    sampleCurrentColor(){
+    	let pixelSample = ctx.getImageData(this.x, this.y, 1, 1);
+    	pixelSample = [
+    		pixelSample.data[0],
+    		pixelSample.data[1],
+    		pixelSample.data[2]
+    	];
+    	let pixelSampleHSL = rgb2hsl(pixelSample);
+    	return pixelSampleHSL;
+    }
 }
 
 // Painter
@@ -110,6 +167,7 @@ class Painter extends Brush {
 		super(color, size, x, y);
 	}
 
+	// Not in use
 	drawCross(sizeX, sizeY){
 		this.setSize(sizeX, sizeY);
 		this.paint();
@@ -134,12 +192,13 @@ class Painter extends Brush {
 // Create the painter object
 let painter = new Painter(colors[1], pixelSize, gridCenter, gridCenter);
 
-
-
 // Random Paint
+let currentSampleColor = painter.sampleCurrentColor();
+
 setInterval(function(){
-	// Paint last dot
-	painter.setColor(colors[4]);
+	// Paint last dot with brighter color
+	nextColor = 'hsl('+currentSampleColor[0]+','+currentSampleColor[1]+'%,'+(currentSampleColor[2] + 1)+'%)';
+	painter.setColor(nextColor);
 	painter.paint();
 
 	// Randomize
@@ -147,6 +206,7 @@ setInterval(function(){
 
 	// Paint
 	painter.step(directions[randDirection], 1);
+	currentSampleColor = painter.sampleCurrentColor();
 	painter.setColor(colors[1]);
 	painter.paint();
-}, 1);
+}, stepInterval);
